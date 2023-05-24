7c4d67d49aec40e50ed17ca4face076145f0c0c2
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-05-24 22:10:19 +0800

    解决路由缓存问题

--- rabbit-shopping/src/views/Category/index.vue
+++ rabbit-shopping/src/views/Category/index.vue
@@ -3,16 +3,23 @@ import GoodsItem from '../Home/components/GoodsItem.vue';
 import { getCategoryAPI } from '@/apis/category';
 import { getBannerAPI } from '@/apis/home';
 import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, onBeforeRouteUpdate } from 'vue-router';
 
 const route = useRoute();
 const categoryData = ref({});
-const getCategory = async () => {
-    const res = await getCategoryAPI(route.params.id);
+const getCategory = async (id = route.params.id) => {
+    const res = await getCategoryAPI(id);
     categoryData.value = res.result;
 };
+// to是到哪
+onBeforeRouteUpdate((to) => {
+    // route.params.id有滞后性
+    // 存在问题：使用最新的路由参数请求最新的分类数据
+    getCategory(to.params.id);
+});
 
 onMounted(() => getCategory());
+//目标：路由参数变化的时候可以把分类数据接口重新发送
 
 const bannerList = ref([]);
 const getBanner = async () => {
