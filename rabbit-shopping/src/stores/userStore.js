9cd1f874d2c0b9423eb45d9a6a0e1d75455e8b21
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-05-28 18:00:28 +0800

    退出清空购物车

--- rabbit-shopping/src/stores/userStore.js
+++ rabbit-shopping/src/stores/userStore.js
@@ -3,12 +3,13 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { loginAPI } from '@/apis/user';
-// import { useCartStore } from './cartStore';
+import { useCartStore } from './cartStore';
 // import { mergeCartAPI } from '@/apis/cart';
 
 export const useUserStore = defineStore(
   'user',
   () => {
+    const cartStore = useCartStore();
     // 1. 定义管理用户数据的state
     const userInfo = ref({});
     // 2. 定义获取接口数据的action函数
@@ -19,6 +20,8 @@ export const useUserStore = defineStore(
 
     const clearUserInfo = () => {
       userInfo.value = {};
+      // 执行清除购物车的action
+      cartStore.clearCart();
     };
     return {
       userInfo,
