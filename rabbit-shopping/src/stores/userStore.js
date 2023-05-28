11fc677d31f3e0515dc984942b29ee262e31deac
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-05-28 18:02:34 +0800

    合并本地购物车

--- rabbit-shopping/src/stores/userStore.js
+++ rabbit-shopping/src/stores/userStore.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { loginAPI } from '@/apis/user';
 import { useCartStore } from './cartStore';
-// import { mergeCartAPI } from '@/apis/cart';
+import { mergeCartAPI } from '@/apis/cart';
 
 export const useUserStore = defineStore(
   'user',
@@ -16,6 +16,17 @@ export const useUserStore = defineStore(
     const getUserInfo = async ({ account, password }) => {
       const res = await loginAPI({ account, password });
       userInfo.value = res.result;
+      // 合并购物车的操作
+      await mergeCartAPI(
+        cartStore.cartList.map((item) => {
+          return {
+            skuId: item.skuId,
+            selected: item.selected,
+            count: item.count
+          };
+        })
+      );
+      cartStore.updateNewList();
     };
 
     const clearUserInfo = () => {
