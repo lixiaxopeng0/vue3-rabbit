72072a0aaab51dd9ff5eccd51dcddd7815bbd16b
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-05-28 20:00:34 +0800

    支付地址添加

--- rabbit-shopping/src/views/Pay/index.vue
+++ rabbit-shopping/src/views/Pay/index.vue
@@ -18,9 +18,9 @@ onMounted(() => getPayInfo());
 
 // 跳转支付
 // 携带订单id以及回调地址跳转到支付地址（get）
-// 支付地址
+// 支付地址:由后端决定
 const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/';
-const backURL = 'http://127.0.0.1:5173/paycallback';
+const backURL = ' http://127.0.0.1:5173/paycallback';
 const redirectUrl = encodeURIComponent(backURL);
 const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`;
 </script>
