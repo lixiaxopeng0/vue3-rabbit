a5320a8408f8676046b88ba15c717516aa78adcb
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-05-28 17:40:04 +0800

    付费展示页面添加

--- rabbit-shopping/src/stores/cartStore.js
+++ rabbit-shopping/src/stores/cartStore.js
@@ -66,6 +66,19 @@ export const useCartStore = defineStore(
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0));
     // 2. 总价 所有项的count*price之和
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0));
+
+    // 3. 已选择数量
+    const selectedCount = computed(() =>
+      cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count, 0)
+    );
+    // 4. 已选择商品价钱合计
+    const selectedPrice = computed(() =>
+      cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count * c.price, 0)
+    );
+
+    // 是否全选
+    const isAll = computed(() => cartList.value.every((item) => item.selected));
+
     return {
       updateNewList,
       addCart,
@@ -73,7 +86,10 @@ export const useCartStore = defineStore(
       clearCart,
       cartList,
       allCount,
-      allPrice
+      allPrice,
+      selectedCount,
+      selectedPrice,
+      isAll
     };
   },
   {
